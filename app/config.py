55e1c2378eeba6e7131390ba09e42f475fcbbc85
javierha <jose.herrera@meltsan.com>
2025-02-07 18:03:14 -0600

    chore(config): Actualizacion en el manejo de errores del archivo config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import sys
 
 class Settings:
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,5 +11,10 @@ class Settings:
     def __init__(self):
         # DB Production
         self.DB_URL = os.getenv('DB_URL')
+
+        if not self.DB_URL:
+            print("ERROR: La variable de entorno DB_URL no está definida.")
+            print("La aplicación no puede iniciarse.")
+            sys.exit(1)
 
 settings = Settings()
